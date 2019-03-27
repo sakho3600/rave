52f48d798dd6f3d8cf8af3d7153bcdaa92a8b822
as1ndu <git@drileba.capital>
2019-03-27 09:18:53 +0300

    added datastructures for mobile money, cards & banking

---
+++ lib/utils/dataStructures.dart
@@ -0,0 +1,48 @@
+/* 
+  *MobileMoney 
+*/
+
+// Uganda
+var mmUG = Map();
+
+// MPESA (TZ & KE)
+var mmMPESA = Map();
+
+// Rwanda
+var mmRW = Map();
+
+// Zambia
+var mmZB = Map();
+
+// Ghana
+var mmGH = Map();
+
+
+/* 
+  * Debit & Credit Card Transactions 
+*/
+
+// generic card information
+var vanillaCards = Map();
+
+// cards tharequire address verification
+var avsCards = Map();
+
+// cards with 3D secure feature
+var threeDSCards = Map();
+
+// Nigerian pin based Mastercard/Verve card
+var ngMCVC = Map();
+
+/*
+  * Bank Payments
+*/
+
+// Charge us bank account via ACH 
+var usBank = Map();
+
+// charge Nigerian banks
+var ngBank = Map();
+
+
+
