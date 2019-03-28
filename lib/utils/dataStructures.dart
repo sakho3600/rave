258678ed0b65258533b0fc371e3d7f3f80d36f9d
as1ndu <git@drileba.capital>
2019-03-28 23:21:57 +0300

    added function to datastructure

--- lib/utils/dataStructures.dart
+++ lib/utils/dataStructures.dart
@@ -1,22 +1,57 @@
+import 'dart:convert';
 /* 
   *MobileMoney 
 */
 
 // Uganda
-var mmUG = Map();
+ mmUG(pubKey, amount, email, phoneNumber, firstName, lastName, ip) {
+  var mmUG = Map();
+  mmUG['PBFPubKey'] = pubKey;
+  mmUG['currency'] = 'UGX';
+  mmUG['payment_type'] = 'mobilemoneyuganda';
+  mmUG['country'] = 'NG';
+  mmUG['amount'] = amount;
+  mmUG['email'] = email;
+  mmUG['phonenumber'] = phoneNumber;
+  mmUG['network'] = 'UGX';
+  mmUG['firstname'] = firstName;
+  mmUG['lastname'] = lastName;
+  mmUG['IP'] = ip;
+  mmUG['txRef'] = 'MC-' + DateTime.now().microsecondsSinceEpoch.toString();
+  mmUG['orderRef'] = 'MC_' + DateTime.now().microsecondsSinceEpoch.toString();
+  mmUG['is_mobile_money_ug'] = 1;
+  mmUG['redirect_url'] = 'https://rave-webhook.herokuapp.com/receivepayment';
+  mmUG['device_fingerprint'] = '69e6b7f0b72037aa8428b70fbe03986c';
+  return json.encode(mmUG);
+}
 
 // MPESA (TZ & KE)
-var mmMPESA = Map();
+Map<String, dynamic> mmMPESA() {
+  var mmMPESA = Map();
+  mmMPESA[''] = '';
+  return mmMPESA;
+}
 
 // Rwanda
-var mmRW = Map();
+Map<String, dynamic> mmRW() {
+  var mmRW = Map();
+  mmRW[''] = '';
+  return mmRW;
+}
 
 // Zambia
-var mmZB = Map();
+Map<String, dynamic> mmZB() {
+  var mmZB = Map();
+  mmZB[''] = '';
+  return mmZB;
+}
 
 // Ghana
-var mmGH = Map();
-
+Map<String, dynamic> mmGH() {
+  var mmGH = Map();
+  mmGH[''] = '';
+  return mmGH;
+}
 
 /* 
   * Debit & Credit Card Transactions 
@@ -38,11 +73,8 @@ var ngMCVC = Map();
   * Bank Payments
 */
 
-// Charge us bank account via ACH 
+// Charge us bank account via ACH
 var usBank = Map();
 
 // charge Nigerian banks
 var ngBank = Map();
-
-
-
